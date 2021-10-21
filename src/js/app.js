d85e7aa9ba77efdb4efcfbdbe7f687a96fec1cdb
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-10-21 18:47:23 +0300

    application done with modal on click and auto scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,98 @@
 import PicturesAPI from './apiService.js';
 import formTemplate from '../templates/form.handlebars';
 import galleryTemplate from '../templates/gallery.handlebars';
-import cardTemplate from '../templates/card.handlebars';
 import { debounce } from 'lodash';
+import { alert, notice, info, success, error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import * as basicLightbox from 'basiclightbox';
 import refs from './refs.js';
 
-const { containerRef } = refs;
+const { containerRef, resultRef, loadMoreBtnRef } = refs;
 
 const picturesAPI = new PicturesAPI();
 
 
-insertTemplate(formTemplate);
+insertTemplate(formTemplate, containerRef);
 
-function insertTemplate(template, data) {
-  const markup = template(data);
-  containerRef.insertAdjacentHTML('beforeend', markup);  
-}
 
 const inputRef = document.querySelector('.search-input');
+inputRef.addEventListener('input', debounce(onInputSearch, 1500));
 
-inputRef.addEventListener('input', debounce(onInputSubmit, 1500))
-function onInputSubmit(event) {
-  picturesAPI.query = event.target.value;
-  
-  renderByFetch()
+
+function onInputSearch(event) {
+  let searchValue = event.target.value;
+  if (searchValue.trim() !== '') {
+    clearPage();
+    loadMoreBtnRef.classList.add('is-hidden');
+    
+    picturesAPI.query = searchValue;
+    renderByFetch()   
+  } return
+}
+
+function insertTemplate(template, place, data) {
+  const markup = template(data);
+  place.insertAdjacentHTML('beforeend', markup);  
 }
 
 async function renderByFetch() {
   try {
     const promise = await picturesAPI.fetchPictures();
     const data = promise.hits;
-
-    insertTemplate(galleryTemplate, data)
+    if (data.length === 0) {
+      showErrorMessage();
+      return;
+    }
+    insertTemplate(galleryTemplate, resultRef, data);
+    resultRef.addEventListener('click', onPictureClick)
+    showLoadMoreBtn();
   } catch (error) {
-    console.log('ОШИБКА!!!', error.name);
+    showErrorMessage();
   }  
 }
 
+function showLoadMoreBtn(){
+  loadMoreBtnRef.classList.remove('is-hidden');
+  loadMoreBtnRef.addEventListener('click', onLoadMoreClick)
+}
+
+function onLoadMoreClick(event) {
+  if (picturesAPI.query === inputRef.value) {
+    picturesAPI.page += 1;
+    renderByFetch();
+    setTimeout(() => {
+      const elems = document.querySelectorAll('ul');
+      
+      elems[elems.length-1].scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
+    }, 1000);    
+  }
+  return
+}
+
+function onPictureClick(event) {
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+  <img src='${event.target.dataset.source}' alt='${event.target.alt}'>
+`)
+  
+  instance.show()
+}
+
 function clearPage() {
+  resultRef.innerHTML= '';
 }
  
+function showErrorMessage() {
+  error({
+    text: 'Nothing was found by your query!',
+    type: 'error',
+    width: '600px',
+    closer: false,
+    sticker: false,
+    delay: 2000
+  })
+}
     
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,6 @@
 export default {
   containerRef: document.querySelector('.container'),
-  
+  resultRef: document.querySelector('.query-result'),
+  loadMoreBtnRef : document.querySelector('.load-more-btn')
 }
 
